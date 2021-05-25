ed79ab5de2d26743c4fef0c8b315f986dc3b7c0b
User <shwetharmaiya@gmail.com>
2021-05-25 14:34:27 +0530

    404 General Error

--- ramble/ramble/settings.py
+++ ramble/ramble/settings.py
@@ -74,7 +74,7 @@ SECURE_SSL_REDIRECT = False
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
