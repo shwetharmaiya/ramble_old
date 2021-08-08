fe38c81d11046f2c5ed75332eaa95e7a4dc4c44f
shwetharmaiya <>
2021-08-08 17:59:20 +0000

    Added all the models to admin console

--- ramble/rambleapp/admin.py
+++ ramble/rambleapp/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Blocked, Collection, CollectionPost, Comment, Follow, HidePost, InterestedUsers, Like, Muted, Profile, Share, TaggableManager
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Blocked)
+admin.site.register(Collection)
+admin.site.register(CollectionPost)
+admin.site.register(Comment)
+admin.site.register(Follow)
+admin.site.register(HidePost)
+admin.site.register(InterestedUsers)
+admin.site.register(Like)
+admin.site.register(Muted)
+admin.site.register(Profile)
+admin.site.register(Share)
